52fb5a268fbe1e8dae82d273b9f6a4105ef75e5b
de1bo <ogsal5765@naver.com>
2022-02-28 17:29:09 +0900

    first

---
+++ src/main/java/com/junefw/infra/modules/member/Member.java
@@ -0,0 +1,37 @@
+package com.junefw.infra.modules.member;
+
+public class Member {
+	private String seq="";
+	private String ifmmId="";
+	private String ifmmName="";
+	private Integer ifmmDelNy;
+	
+	// ------------------
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getIfmmId() {
+		return ifmmId;
+	}
+	
+	public void setIfmmId(String ifmmId) {
+		this.ifmmId = ifmmId;
+	}
+	public String getIfmmName() {
+		return ifmmName;
+	}
+	public void setIfmmName(String ifmmName) {
+		this.ifmmName = ifmmName;
+	}
+	
+	public Integer getIfmmDelNy() {
+		return ifmmDelNy;
+	}
+	public void setIfmmDelNy(Integer ifmmDelNy) {
+		this.ifmmDelNy = ifmmDelNy;
+	}
+	
+}
